007e369770024adeaa285ab09ac41cc3e86aba51
Aaron <AaronBatilo@gmail.com>
2017-09-29 22:57:35 -0600

    Implement some sample OpenNLP code

--- sbtk/src/main/java/WordScanner.java
+++ sbtk/src/main/java/WordScanner.java
@@ -1,5 +1,36 @@
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
+import opennlp.tools.tokenize.SimpleTokenizer;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 public class WordScanner {
-    public static String[] split(String s) {
-        return s.split("\\s+");
+    private static SentenceDetectorME detector;
+    static {
+        final InputStream modelStream =
+                WordScanner.class.getResourceAsStream("en-sent.bin");
+        try {
+            final SentenceModel model = new SentenceModel(modelStream);
+            detector = new SentenceDetectorME(model);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (modelStream != null) {
+                try {
+                    modelStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    static String[] split(String s) {
+        return SimpleTokenizer.INSTANCE.tokenize(s);
+    }
+
+    public static String[] sentence(String s) throws IOException {
+        return detector.sentDetect(s);
     }
 }
--- sbtk/src/test/java/WordScannerTest.java
+++ sbtk/src/test/java/WordScannerTest.java
@@ -1,14 +1,22 @@
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.assertArrayEquals;
 
 public class WordScannerTest {
     @Test
     public void split() throws Exception {
-        String s1 = "This is a test";
+        String s1 = "This is a single sentence. Here, and now, is a second sentence with more words?";
+        System.out.println(Arrays.toString(WordScanner.split(s1)));
         String[] expected = new String[] {
                 "This", "is", "a", "test"
         };
         assertArrayEquals(expected, WordScanner.split(s1));
+    }
+
+    @Test
+    public void sentence() throws Exception {
+        System.out.println(Arrays.toString(WordScanner.sentence("Interviewing is too blind. Writing software is so much more than just the act of writing code. If you want to write software professionally, you’re talking about planning, debugging, writing, testing, bug reporting, all sorts of things. But most people will only interview on two things: culture fit, and technical ability in its broadest definition. It’s impossible to be able to measure all of that, let alone in a reasonable amount of time to ask of an interviewee.")));
     }
 }
