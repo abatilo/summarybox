29393248eb27c54d787ff25882b05635df96e57c
Aaron <AaronBatilo@gmail.com>
2017-10-07 17:29:11 -0600

    Embed the stopwords

--- summarybox-service/src/main/java/BaseResourceFactory.java
+++ summarybox-service/src/main/java/BaseResourceFactory.java
@@ -1,12 +1,11 @@
 import com.google.common.collect.Sets;
-import com.google.common.io.Files;
 import com.google.common.io.Resources;
 import io.dropwizard.setup.Environment;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.util.Set;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import opennlp.tools.postag.POSModel;
@@ -25,25 +24,26 @@ public class BaseResourceFactory {
       super(config, env);
     }
 
-    private final InputStream sentenceStream =
+    @Getter(AccessLevel.NONE) private final String w2vModel = config.getW2vModel();
+    @Getter(AccessLevel.NONE) private final Word2Vec vec =
+        WordVectorSerializer.readWord2VecModel(w2vModel);
+    @Getter(AccessLevel.NONE) private final InputStream sentenceStream =
         Resources.getResource(config.getSentenceModel()).openStream();
-    private final SentenceModel sentenceModel = new SentenceModel(sentenceStream);
-    private final ThreadLocalDetector detector = new ThreadLocalDetector(sentenceModel);
-
-    private final InputStream posStream =
+    @Getter(AccessLevel.NONE) private final SentenceModel sentenceModel =
+        new SentenceModel(sentenceStream);
+    @Getter(AccessLevel.NONE) private final ThreadLocalDetector detector =
+        new ThreadLocalDetector(sentenceModel);
+    @Getter(AccessLevel.NONE) private final InputStream posStream =
         Resources.getResource(config.getPosModel()).openStream();
-    private final POSModel posModel = new POSModel(posStream);
-    private final ThreadLocalTagger tagger = new ThreadLocalTagger(posModel);
+    @Getter(AccessLevel.NONE) private final POSModel posModel = new POSModel(posStream);
+    @Getter(AccessLevel.NONE) private final ThreadLocalTagger tagger =
+        new ThreadLocalTagger(posModel);
+    @Getter(AccessLevel.NONE) private final Set<String> STOP_WORDS = Sets.newHashSet(
+        Resources.readLines(Resources.getResource(config.getStopWords()),
+            Charset.defaultCharset()));
+    @Getter(AccessLevel.NONE) private final WordScanner scanner =
+        new WordScanner(detector, tagger, vec, STOP_WORDS);
 
-    private final String w2vModel = config.getW2vModel();
-    private final Word2Vec vec = WordVectorSerializer.readWord2VecModel(w2vModel);
-
-    private final Set<String> STOP_WORDS =
-        Sets.newHashSet(
-            Files.readLines(new File(config.getStopWords()),
-                Charset.defaultCharset()));
-
-    private final WordScanner scanner = new WordScanner(detector, tagger, vec, STOP_WORDS);
     private final RootResource rootResource = new RootResource(scanner);
   }
 }
